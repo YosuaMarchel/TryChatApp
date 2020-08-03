46c28e20bc324126ced843d9f00988e5087ec5e2
Yosua Marchel <yosua.marchel@gmail.com>
2020-08-04 04:12:30 +0700

    getting started

---
+++ Try Chat App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Try Chat App
+//
+//  Created by Yosua Marchel on 04/08/20.
+//  Copyright © 2020 Yosua Marchel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
