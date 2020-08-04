c445f07484535358a497411bc183377f3a9b4df6
Yosua Marchel <yosua.marchel@gmail.com>
2020-08-04 14:57:48 +0700

    handle auth session, add database

--- Try Chat App/Controllers/ConversationsViewController.swift
+++ Try Chat App/Controllers/ConversationsViewController.swift
@@ -7,9 +7,10 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ConversationsViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
@@ -17,16 +18,18 @@ class ConversationsViewController: UIViewController {
     
     override func viewDidAppear(_ animated: Bool){
         super.viewDidAppear(animated)
-        
-        let isLoggedIn = UserDefaults.standard.bool(forKey: "logged_in")
-        
-        if !isLoggedIn{
+                
+        validateAuth()
+    }
+    
+    private func validateAuth() {
+        if FirebaseAuth.Auth.auth().currentUser == nil {
             let vc = LoginViewController()
             let nav = UINavigationController(rootViewController: vc)
             nav.modalPresentationStyle = .fullScreen
             present(nav, animated: false)
         }
     }
-
+    
 }
 
--- Try Chat App/Controllers/Login/LoginViewController.swift
+++ Try Chat App/Controllers/Login/LoginViewController.swift
@@ -126,7 +126,10 @@ class LoginViewController: UIViewController {
         }
         
         //Firebase log in
-        FirebaseAuth.Auth.auth().signIn(withEmail: email, password: password, completion: { authResult, error in
+        FirebaseAuth.Auth.auth().signIn(withEmail: email, password: password, completion: { [weak self] authResult, error in
+            guard let strongSelf = self else {
+                return
+            }
             guard let result = authResult, error == nil else {
                 print("Failed to Log in with email: \(email)")
                 return
@@ -134,6 +137,7 @@ class LoginViewController: UIViewController {
             
             let user = result.user
             print("Log in user: \(user)")
+            strongSelf.navigationController?.dismiss(animated: true, completion: nil)
         })
     }
     
